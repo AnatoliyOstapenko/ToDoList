c22e2a685e7fc334f1d1b6b5641dd8b7bca739e9
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-08-21 00:39:50 +0300

    add UITableViewDataSource

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+// change UIViewController to UITableViewController
 class ToDoTableViewController: UITableViewController {
-
+    
+    // create array to use in table
+    let itemArray = ["1", "2", "3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+}
 
+//MARK: - UITableViewDataSource
 
+extension ToDoTableViewController: UITableViewDataSource {
+    
+    // set numbers of rows in TableView
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        
+    }
+    
+    // set cell and changing UI by TableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create message to get acces to row in Table View
+        let item = itemArray[indexPath.row]
+        
+        // Returns a reusable table-view cell object as! MessageCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath) as! ToDoTableViewController
+        
+        
+        return cell
+        
+        
+    }
 }
 

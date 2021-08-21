a996f86e8da5d00152238532dda2d6e02034ed63
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-08-21 17:55:48 +0300

    live version for TableView

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -10,40 +10,48 @@ import UIKit
 // change UIViewController to UITableViewController
 class ToDoTableViewController: UITableViewController {
     
+    @IBOutlet var toDoTableView: UITableView!
+
+    
+    
+    
     // create array to use in table
     let itemArray = ["1", "2", "3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
-    
+    //MARK: - UITableViewDataSource
+
+
+        
+        // set numbers of rows in TableView
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
+            
+        }
+        
+        // ask for the data source for a cell to insert in a particular location of the table view
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            // create proper item to get acces to row in Table View
+            let item = itemArray[indexPath.row]
+            
+            // Return a reusable table view cell object as! MessageCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath) as! ToDoTableViewCell
+            
+            cell.textLabel?.text = "String(item[0])"
+
+
+            return cell
+            
+            
+        }
     
 }
 
-//MARK: - UITableViewDataSource
 
-extension ToDoTableViewController: UITableViewDataSource {
-    
-    // set numbers of rows in TableView
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-        
-    }
-    
-    // set cell and changing UI by TableViewCell
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // create message to get acces to row in Table View
-        let item = itemArray[indexPath.row]
-        
-        // Returns a reusable table-view cell object as! MessageCell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath) as! ToDoTableViewController
-        
-        
-        return cell
-        
-        
-    }
-}
+
 

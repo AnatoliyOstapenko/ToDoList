44fcada8756f3281e96a5aa6365208e4f20d0715
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-08-23 22:16:32 +0300

    add ToDoModel struct

--- ToDoList/Controller/ToDoTableViewController.swift
+++ ToDoList/Controller/ToDoTableViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 // change UIViewController to UITableViewController (UITableViewDataSource and UITableViewDelegate included)
 class ToDoTableViewController: UITableViewController {
     
+    // create array to initialize for using in table view
+    var itemArray = [ToDoModel]()
+    
     // create defaults to use data persistence
     let defaults = UserDefaults.standard
 
-    
-    // create array to use in table view
-    var itemArray: [String] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //retrieve a last data (array) after closing app from local data persistence
-        if let savedData = defaults.array(forKey: "ToDoKey") as? [String] {
-            itemArray = savedData
-        }
+        //activate ToDoModel
+        var newItem = ToDoModel()
+        newItem.title = "First Line"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        var newItem2 = ToDoModel()
+        newItem2.title = "Second Line"
+        newItem2.done = false
+        itemArray.append(newItem2)
+        
+        
+//        //retrieve a last data from array after closing app from local data persistence
+//        if let item = defaults.array(forKey: "ToDoKey") as? [String] {
+//
+//            //dispatch data to array
+//            itemArray = item
+//    }
 
         // switch to Light Mode screen (avoid dark background table view)
         overrideUserInterfaceStyle = .light
@@ -39,38 +52,43 @@ class ToDoTableViewController: UITableViewController {
         
         // ask for the data source for a cell to insert in a particular location of the table view
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
             
             // create item to get acces to row in itemArray of Table View
             let item = itemArray[indexPath.row]
             
-            // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table.
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-            
             // dispatch to default text label list of text from array
-            cell.textLabel?.text = item
+            cell.textLabel?.text = item.title
+            
+            // set a mark accessory when user toggles a row and unmark when user untoggle a row
+            if item.done == true {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            
             return cell
-            
-            
         }
    
     //MARK: - UITableViewDataDelegate
     
     // delegate to create an interaction UI with tableview, when user select row
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("did select row: \(itemArray[indexPath.row])")
-
         
-        // create let call to unwrap optional cellForRow
-        guard let cell = tableView.cellForRow(at: indexPath) else {
-            return
-        }
-        // set a mark accessory when user toggles a row and unmark when user untoggle a row
-        if cell.accessoryType == .checkmark {
-            cell.accessoryType = .none
+        // retrieve "done" from array by ToDoModel
+        var item = itemArray[indexPath.row].done
+        
+        //change "done" on true or false when user select a row
+        if item == false {
+            item = true
         } else {
-            cell.accessoryType = .checkmark
+            item = false
         }
-
+        
+        // reload data in table view to change "done"
+        tableView.reloadData()
         
         // create animated effect of deselecting row
         tableView.deselectRow(at: indexPath, animated: true)
@@ -90,15 +108,22 @@ class ToDoTableViewController: UITableViewController {
         // add mandatory next step. create action for alert message
         let action = UIAlertAction(title: "\u{2705}", style: .default) { (action) in
             
-            // All this happens when user click on button
+            // MARK: - All this happens when user click on button
             // unwrap optional text from TextField
             guard let newItem = textField.text else {
                 return
             }
-            // add new item that user type to array
-            self.itemArray.append(newItem)
             
-            // save last list of array properties
+            //set a new item to initialize struct ToDoModel
+            var toDoModel = ToDoModel()
+            
+            //get from text field by newItem what user printed
+            toDoModel.title = newItem
+            
+            // add new printed text further that user type to array
+            self.itemArray.append(toDoModel)
+            
+            // save last data (array properties)
             self.defaults.set(self.itemArray, forKey: "ToDoKey")
             
             // update array - reload data in table view to add a new row
@@ -113,15 +138,6 @@ class ToDoTableViewController: UITableViewController {
             alertTextField.placeholder = "create a new item"
             textField = alertTextField
             
-//            guard let item = alertTextField.text else {
-//                return
-//            }
-//
-//            // create Capture Value to get value from closure
-//            func getNewItem() -> String {
-//                return item
-//            }
-//            return getNewItem()
         }
         
         // attaches an action object to the alert or action sheet.
---
+++ ToDoList/Model/ToDoModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoModel.swift
+//  ToDoList
+//
+//  Created by MacBook on 23.08.2021.
+//
+
+import Foundation
+
+struct ToDoModel {
+    
+    var title: String = ""
+    var done: Bool = true
+    
+    
+}
